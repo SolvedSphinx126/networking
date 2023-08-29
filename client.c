d7b3d24019efc42690f2c1d7f75f9db870d248f5
Ben O'Bryan <obryanbw@gmail.com>
2023-08-28 22:48:09 -0500

    baby's first words

--- client.c
+++ client.c
@@ -4,16 +4,43 @@
 #include <sys/types.h>
 #include <netinet/ip.h>
 #include <errno.h>
+#include <stdlib.h>
 
 #define BUF_SIZE 500
 
+int getPort(in_port_t port) {
+    uint16_t a = (port << 8);
+    uint16_t b = (port >> 8);
+    return (int) a | b;
+}
+
+char * getIp(in_addr_t addr) {
+    u_int8_t a = addr & 255;
+    addr >>= 8;
+
+    u_int8_t b = addr & 255;
+    addr >>= 8;
+
+    u_int8_t c = addr & 255;
+    addr >>= 8;
+
+    u_int8_t d = addr & 255;
+    char* ret = malloc(sizeof(char) * 15);
+    sprintf(ret, "%d.%d.%d.%d", a, b, c, d);
+    return ret;
+    // leaks memory don't care yet tho
+}
 
 int main() {
     int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     printf("Create socket fd: %s\n", strerror(errno));
 
-    const struct sockaddr addr = {AF_INET, };
-    //addr.sa_family = AF_INET;
-    //addr.sa_data = "127.0.0.1";
-    //connect(sock, );
+    printf("addr: %s\n", getIp(0x79351DAC));
+    printf("port: %d\n", getPort(0x5f81));
+
+    struct sockaddr_in addr = {AF_INET, 0x5f81, 0x79351DAC};
+    connect(sock, (struct sockaddr *) &addr, sizeof(addr));
+    int test = errno;
+    printf("Connect to server: %s\n", strerror(test));
+    printf("Connect to server: %s\n", strerrorname_np(test));
 }
--- server.c
+++ server.c
@@ -1,4 +1,7 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -7,22 +10,53 @@
 
 #define BUF_SIZE 500
 
+int getPort(in_port_t port) {
+    uint16_t a = (port << 8);
+    uint16_t b = (port >> 8);
+    return (int) a | b;
+}
+
+char * getIp(in_addr_t addr) {
+    u_int8_t a = addr & 255;
+    addr >>= 8;
+
+    u_int8_t b = addr & 255;
+    addr >>= 8;
+
+    u_int8_t c = addr & 255;
+    addr >>= 8;
+
+    u_int8_t d = addr & 255;
+    char* ret = malloc(sizeof(char) * 15);
+    sprintf(ret, "%d.%d.%d.%d", a, b, c, d);
+    return ret;
+    // leaks memory don't care yet tho
+}
 
 int main() {
     int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     printf("Create socket fd: %s\n", strerror(errno));
 
     const struct sockaddr_in addr = {};
-    bind(sock, &addr, (socklen_t) sizeof(addr));
+    
+    
+    bind(sock, (struct sockaddr *restrict) &addr, (socklen_t) sizeof(addr));
     printf("Bind socket to addr: %s\n", strerror(errno));
 
     listen(sock, 16);
     printf("Start listening: %s\n", strerror(errno));
 
-    getsockname(sock, (struct sockaddr *restrict) &addr, (socklen_t *restrict) sizeof(addr));
-    printf("addr: %d\n", addr.sin_addr.s_addr);
-    printf("addr: %d\n", addr.sin_port);
+    struct sockaddr_in addr2 = {};
+    int sa_len;
+    sa_len = sizeof(addr2);
+    getsockname(sock, (struct sockaddr_in *) &addr2, (socklen_t *restrict) &sa_len);
+    printf("addr: %s\n", getIp(addr2.sin_addr.s_addr));
+    printf("port: %d\n", getPort(addr2.sin_port));
+    printf("Get sock info: %s\n", strerror(errno));
 
-    accept(sock, (struct sockaddr *restrict) &addr, (socklen_t *restrict) sizeof(addr));
+    struct sockaddr_in client = {};
+    sa_len = sizeof(client);
+    accept(sock, (struct sockaddr *restrict) &client, (socklen_t *restrict) &sa_len);
     printf("Accept message: %s\n", strerror(errno));
 }
+
