f1ece074ccee49321a15c7550beecf88f2fe4650
Ben O'Bryan <obryanbw@gamil.com>
2023-10-25 04:18:01 +0000

    perform massive refactor to support poll()

--- server.c
+++ server.c
@@ -1,101 +1,181 @@
-#define _GNU_SOURCE
-
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <poll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <netdb.h>
 #include <netinet/ip.h>
 #include <errno.h>
 #include <arpa/inet.h>
 
-#define BUF_SIZE 500
+#define BUF_SIZE 100
 
-struct client {
-    struct sockaddr_in addr;
-    int fd;
-};
+unsigned int users = 0;
 
-int getPort(in_port_t port) {
-    uint16_t a = (port << 8);
-    uint16_t b = (port >> 8);
-    return (int) a | b;
-}
-
-char * getIp(in_addr_t addr) {
-    u_int8_t a = addr & 255;
-    addr >>= 8;
-
-    u_int8_t b = addr & 255;
-    addr >>= 8;
-
-    u_int8_t c = addr & 255;
-
-    addr >>= 8;
-
-    u_int8_t d = addr & 255;
-    char* ret = malloc(sizeof(char) * 15);
-    sprintf(ret, "%d.%d.%d.%d", a, b, c, d);
-    return ret;
-    // leaks memory don't care yet tho
+struct pollfd acceptNewClient() {
 }
 
 int initServer() {
-    int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    printf("Create socket fd: %s\n", strerror(errno));
 
-    const struct sockaddr_in addr = {};
-    
-    bind(sock, (struct sockaddr *restrict) &addr, (socklen_t) sizeof(addr));
-    printf("Bind socket to addr: %s\n", strerror(errno));
+    struct addrinfo hints, *res;
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = 0;
 
-    listen(sock, 16);
-    printf("Start listening: %s\n", strerror(errno));
+    char ip4[INET_ADDRSTRLEN];
+    char ip6[INET6_ADDRSTRLEN];
+    int status;
 
-    struct sockaddr_in addr2 = {};
-    int sa_len;
-    sa_len = sizeof(addr2);
-    getsockname(sock, (struct sockaddr_in *) &addr2, (socklen_t *restrict) &sa_len);
-    printf("addr: %s\n", getIp(addr2.sin_addr.s_addr));
-    printf("port: %d\n", getPort(addr2.sin_port));
-    printf("Get sock info: %s\n", strerror(errno));
-    return sock;
+    if ((status = getaddrinfo(NULL, "12312", &hints, &res)) != 0) {
+        printf("%s\n", gai_strerror(status));
+    } else {
+        printf("Got info\n");
+        if(res->ai_addr->sa_family == AF_INET) {
+            if (inet_ntop(AF_INET, &((struct sockaddr_in *) res->ai_addr)->sin_addr, ip4, INET_ADDRSTRLEN)) {
+                printf("ipv4 = %s\n", ip4);
+            } else {
+                printf("%s\n", gai_strerror(errno));
+            }
+        } else {
+            if (inet_ntop(AF_INET6, &((struct sockaddr_in6 *) res->ai_addr)->sin6_addr, ip6, INET6_ADDRSTRLEN)) {
+                printf("ipv6 = %s\n", ip6);
+            } else {
+                printf("%s\n", gai_strerror(errno));
+            }
+        }
+    }
+
+    int s = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if(errno != 0){
+        printf("Create socket: %s\n", gai_strerror(errno));
+        exit(1);
+    } else {
+        printf("Create socket: Success\n");
+    }
+
+    int port = 0;
+    int yes = 1;
+    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+    bind(s, res->ai_addr, res->ai_addrlen);
+    if (errno != 0)
+    {
+        printf("Bind socket: %s %d\n", gai_strerror(errno), errno);
+        exit(1);
+    } else {
+        if(res->ai_family == AF_INET) {
+            port = (int)(ntohs(((struct sockaddr_in *)res->ai_addr)->sin_port));
+        } else {
+            port = (int)(ntohs(((struct sockaddr_in6 *)res->ai_addr)->sin6_port));
+        }
+    }
+    printf("Port: %d\n", port);
+
+    // need error handling`
+    listen(s, 20);
+    return s;
 }
 
-struct client acceptClient(int* serverFd) {
-    struct client c = {};
-    int sa_len = sizeof(c.addr);
-    c.fd = accept(*serverFd, (struct sockaddr *restrict) &c.addr, (socklen_t *restrict) &sa_len);
-    printf("Accept connection: %s\n", strerror(errno));
-    return c;
+void getIp(struct sockaddr *addr, char* ip) {
+    if(addr->sa_family == AF_INET) {
+        inet_ntop(addr->sa_family, &((struct sockaddr_in *)addr)->sin_addr.s_addr, ip, INET6_ADDRSTRLEN);
+        if (errno != 0) {
+            printf("Get printable ip of new ipv4 client: %s\n", gai_strerror(errno));
+            exit(1);
+        }
+    } else {
+        inet_ntop(addr->sa_family, &((struct sockaddr_in6 *)addr)->sin6_addr.s6_addr, ip, INET6_ADDRSTRLEN);
+        if (errno != 0) {
+            printf("Get printable ip of new ipv6 client: %s\n", gai_strerror(errno));
+            exit(1);
+        }
+    }
 }
 
-// needs to handle message that's too long
-char* recieveMessage(int bufSize, struct client c) {
-    char* strBuf = malloc(bufSize*sizeof(char));
-    struct sockaddr_in sender = {};
-    int temp = sizeof(sender);
-    if (recv(c.fd, strBuf, bufSize, 0) > 0) {
-        char* res = malloc(strlen(strBuf) + 1);
-        strcpy(res, strBuf);
-        free(strBuf);
-        return res;
-    }
-    else {
-        printf("Recieve message error: %s\n", strerror(errno));
-        return NULL;
-    }
+int addUser(struct pollfd **pollList, int **userIds, int addedUserFd, int *numUsers, int *nextId) {
+    *pollList = reallocarray(*pollList, (*numUsers) + 1, sizeof(struct pollfd));
+    *userIds = reallocarray(*userIds, (*numUsers) + 1, sizeof(int));
+    // should really check for errors here
+    struct pollfd addedPollFd = {};
+    (*pollList)[*numUsers].fd = addedUserFd;
+    (*pollList)[*numUsers].events = POLLIN;
+    (*userIds)[*numUsers] = *nextId;
+    (*nextId)++;
+    (*numUsers)++;
+    return (*nextId) - 1;
+}
+
+//TODO Crashes when there are 3 users and user 2 quits
+//maybe I can't change poll list at the time that I do?
+void rmUser(struct pollfd **pollList, int **userIds, int iToRemove, int *numUsers) {
+    close((*pollList)[iToRemove].fd);
+    (*pollList)[iToRemove] = (*pollList)[(*numUsers) - 1];
+    (*userIds)[iToRemove] = (*userIds)[(*numUsers) - 1];
+    (*numUsers)--;
 }
 
 int main() {
-    int serverFd = initServer();
-    char* client_ip_addr = malloc(16 * sizeof(char));
-    while (1) {
-        struct client c = acceptClient(&serverFd);
-        char* msg = recieveMessage(500, c);
-	inet_ntop(AF_INET, &(c.addr.sin_addr), client_ip_addr, 16);
-        printf("%s: %s\n", client_ip_addr, msg);
-        free(msg);
+    int users = 0;
+    int nextId = 0;
+    int server = initServer();
+    struct pollfd *usersList;
+    int *userIds;
+    addUser(&usersList, &userIds, server, &users, &nextId);
+
+    //int server = pollsocks[0].fd;
+    char ip[INET6_ADDRSTRLEN] = "";
+    socklen_t addrsize;
+    int fd;
+    int bytesRecieved = 0;
+    int bufSize = 512;
+    char buffer[bufSize];
+    struct sockaddr *addr = &(struct sockaddr) {0};
+
+    // Move common vars out of loop for efficiency
+    int pollCount = 0;
+    while(1) {
+        memset(buffer, '\0', bufSize);
+        int pollCount = poll(usersList, users, -1);
+        if (pollCount == -1) {
+            printf("Poll: %s%d\n", gai_strerror(errno), errno);
+            exit(1);
+        } else {
+            for(int i = 0; i < users; i++) {
+                fd = usersList[i].fd;
+                if(usersList[i].revents & POLLIN) {
+                    if(usersList[i].fd == server) {
+                        addrsize = sizeof(struct sockaddr);
+                        int newUserFd = accept(usersList[i].fd, (struct sockaddr *)addr, &addrsize);
+                        if (errno != 0) {
+                            printf("Accept new client: %s %s\n", gai_strerror(errno), strerror(errno));
+                            exit(1);
+                        } else {
+                            getIp(addr, ip);
+                        }
+                        int uid = addUser(&usersList, &userIds, newUserFd, &users, &nextId);
+                        printf("new user at ip=%s with user id %d!\n", ip, uid);
+                    } else {
+                        bytesRecieved = recv(usersList[i].fd, buffer, bufSize, 0);
+                        if(bytesRecieved < 0) {
+                            printf("Existing client recv: %s %s\n", gai_strerror(errno), strerror(errno));
+                        } else if(bytesRecieved == 0) {
+                            //usersList[i] = usersList[users--];
+                            printf("User %d has disconnected from the server.\n", userIds[i]);
+                            rmUser(&usersList, &userIds, i, &users);
+                        } else {
+                            printf("User %d (ip: %s) sent: %s\n", userIds[i], ip, buffer);
+                            if(strcmp(buffer, "ls") == 0) {
+                                dprintf(usersList[i].fd, "List Time!!\n");
+                                fflush(stdout);
+                            }
+                            dprintf(usersList[i].fd, "You are user %d and you said \"%s\"", userIds[i], buffer);
+                            // handle errors
+                        }
+                    }
+                }
+            }
+        }
     }
 }
-    
